93be389671dbc1c369566e8af2836ed0e2a252e8
henryswerve <120536924+henryswerve@users.noreply.github.com>
2022-12-14 17:32:25 -0800

    i like turtles. testing push

--- uno.java
+++ uno.java
@@ -1,5 +1,16 @@
 public class uno {
     public static void main(String args[]) {
         System.out.println("Hello world");
+        main();
+    }
+    public static void main() {
+        for(int i=1; i<=10; i++) {
+            if (i<3) {
+                System.out.println("Um, actually,");
+            } else {
+                System.out.println("I don't like turtles.");
+            }
+            System.out.println("I like turtles.");
+        }
     }
 }
